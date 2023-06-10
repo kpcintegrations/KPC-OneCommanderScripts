5c61bf7b9715da6ea09d5ac6819f45d8f2fda6fa
Kyle Condon <admin@kpcintegrations.com>
2023-06-10 14:00:41 -0400

    Test

--- OC/Invoke-OCInit.ps1
+++ OC/Invoke-OCInit.ps1
@@ -4,8 +4,10 @@ if (!(Test-Path -Path "C:\Program Files\PowerShell\Scripts\")) {
 New-Item -Path $PSScriptRoot -Name 'Export' -ItemType 'Directory' -Force
 
 $RawProfileContent = @'
-
-$CustomVariables = Import-Clixml -Path ($PSScriptRoot + '\Export\CusVars.xml')
+$CustomVariables = Import-Clixml -Path ('C:\Program Files\PowerShell\Scripts\OC\Export\CusVars.xml')
 $CustomVariables.GetEnumerator() | ForEach-Object -Process { New-Variable -Name $_.Name -Value $_.Value -Option AllScope -Scope Global -Force }
 '@
+Add-Content -Path $PROFILE -Value ""
 Add-Content -Path $PROFILE -Value $RawProfileContent
+$VarHash = @{}
+$VarHash | Export-Clixml -Path ($PSScriptRoot + '\Export\CusVars.xml')
