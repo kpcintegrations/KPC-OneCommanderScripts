0c0ed0665e7a30839954e7d6ea03a57f1a6e4228
Kyle Condon <admin@kpcintegrations.com>
2023-06-10 20:41:22 -0400

    Test

---
+++ OC/Merge-Directories.ps1
@@ -0,0 +1,7 @@
+Write-Host "Enter A Folder Path To Merge Directories To"
+$Folder = Read-Host
+$VarClixml = Import-Clixml -Path ".\Export\Vars.xml"
+$CurDir = $VarClixml["CurrentDir"]
+$OpDir = $VarClixml["OpDir"]
+Copy-Item -Path "$CurDir\*" -Destination $Folder -Recurse
+Copy-Item -Path "$OpDir\*" -Destination $Folder -Recurse
