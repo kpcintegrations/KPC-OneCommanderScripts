77346f9a18e7b14aa8f20d05e6fc906e2a2577ba
Kyle Condon <admin@kpcintegrations.com>
2023-07-10 16:48:10 -0400

    Test

--- OC/Pull-ADBFolder.ps1
+++
@@ -1,4 +0,0 @@
-$OCVars = (Import-Clixml -Path $PSScriptroot'\Export\Vars.xml' )
-$CurDir = $OCVars["CurrentDir"]
-$ADBFolder = Read-Host -Prompt "Enter the User folder you wish to copy to the current directory"
-adb pull $ADBFolder $CurDir
