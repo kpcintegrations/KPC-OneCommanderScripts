4008d2b18a56aeb7607dd9d8eafabb979352a650
Kyle Condon <admin@kpcintegrations.com>
2023-09-30 12:46:50 -0500

    Test

---
+++ OC/Invoke-Terminals.ps1
@@ -0,0 +1,36 @@
+$wtContentJson = Get-Content -Path "C:\Users\kylep\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+$wtContentObj = $wtContentJson | ConvertFrom-Json
+$promptGuids = $wtContentObj.profiles.list.guid
+$promptNames = $wtContentObj.profiles.list.name
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$mainForm = New-Object System.Windows.Forms.Form
+$mainForm.Size = New-Object System.Drawing.Size(500,850)
+$mainForm.StartPosition = "CenterScreen"
+
+$listBox = New-Object System.Windows.Forms.ListBox
+$listBox.Size = New-Object System.Drawing.Size(450,650)
+$listBox.Location = New-Object System.Drawing.Size(25,25)
+foreach ($promptName in $promptNames) {
+$listBox.Items.Add($promptName)
+}
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Text = "Launch Prompt"
+$okButton.Size = New-Object System.Drawing.Size(100,50)
+$okButton.Location = New-Object System.Drawing.Size(200,750)
+
+$mainForm.Controls.Add($listBox)
+$mainForm.Controls.Add($okButton)
+
+$Global:LaunchTerminalCmd = {
+    wt -p $promptGuids[$listBox.SelectedIndex]
+    $mainForm.Close()
+}
+
+$okButton.Add_Click($Global:LaunchTerminalCmd)
+
+
+$mainForm.ShowDialog()
+
