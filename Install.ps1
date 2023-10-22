d809700c9747c065d04312d3d8e52d981acfdf65
Kyle Condon <admin@kpcintegrations.com>
2023-10-21 21:12:15 -0500

    Updated Install Script To Account For Different OC Install Methods

--- Install.ps1
+++ Install.ps1
@@ -12,8 +12,17 @@ if (!(Get-Command pwsh.exe -CommandType Application -ErrorAction SilentlyContinu
 else {
     Write-Host "PowerShell 7 Already Installed"
 }
+$ParseOCPath = ""
 $OCPath = (Get-Process OneCommander).Path
+if ($OCPath.Contains("WindowsApp")) {
+    $ParseOCPath = "$Env:USERPROFILE\OneCommander\Resources\Scripts\"
+}
+elseif ($OCPath.Contains("Program Files")) {
+    $ParseOCPath = "$Env:LOCALAPPDATA\OneCommander\Resources\Scripts\"
+}
+else {
 $ParseOCPath = (Get-Item -LiteralPath $OCPath).Directory.FullName + '\Resources\Scripts\'
+}
 $ParseParseOCPath = ($ParseOCPath | Join-Path -ChildPath "..\KPC\")
 New-Item -Path $ParseOCPath -ItemType Directory -Force
 New-Item -Path $ParseParseOCPath -ItemType Directory -Force
