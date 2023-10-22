8e5b9f8227f51459d74ca8aae08b852a64e3bbe9
Kyle Condon <admin@kpcintegrations.com>
2023-10-22 02:12:10 -0500

    Updated Install Script To Account For Different OC Install Methods

--- OC/Get-AndroidInstalledAppsInfo.ps1
+++ OC/Get-AndroidInstalledAppsInfo.ps1
@@ -22,27 +22,27 @@ public class DPIAware {
 }
 '
 $mainForm = New-Object System.Windows.Forms.Form
-$mainForm.Size = New-Object System.Drawing.Size(650,250)
+$mainForm.Size = New-Object System.Drawing.Size(650, 250)
 $mainForm.StartPosition = "CenterScreen"
 
 $label = New-Object System.Windows.Forms.Label
-$label.Size = New-Object System.Drawing.Size(600,25)
-$label.Location = New-Object System.Drawing.Size(25,25)
+$label.Size = New-Object System.Drawing.Size(600, 25)
+$label.Location = New-Object System.Drawing.Size(25, 25)
 $label.Text = "Select which packages to list"
 
 $checkbox1 = New-Object System.Windows.Forms.CheckBox
-$checkbox1.Size = New-Object System.Drawing.Size(200,25)
-$checkbox1.Location = New-Object System.Drawing.Size(100,75)
+$checkbox1.Size = New-Object System.Drawing.Size(200, 25)
+$checkbox1.Location = New-Object System.Drawing.Size(100, 75)
 $checkbox1.Text = "Third Party Packages"
 
 $checkbox2 = New-Object System.Windows.Forms.CheckBox
-$checkbox2.Size = New-Object System.Drawing.Size(200,25)
-$checkbox2.Location = New-Object System.Drawing.Size(350,75)
+$checkbox2.Size = New-Object System.Drawing.Size(200, 25)
+$checkbox2.Location = New-Object System.Drawing.Size(350, 75)
 $checkbox2.Text = "System Packages"
 
 $button = New-Object System.Windows.Forms.Button
-$button.Size = New-Object System.Drawing.Size(200,50)
-$button.Location = New-Object System.Drawing.Size(225,125)
+$button.Size = New-Object System.Drawing.Size(200, 50)
+$button.Location = New-Object System.Drawing.Size(225, 125)
 $button.Text = "OK"
 New-Variable -Name "ADBArgs" -Value "" -Scope Global
 $buttonSB = {
@@ -86,37 +86,58 @@ if (Test-Path "$PSScriptRoot\Export\ApkFiles\") {
 }
 New-Item -Path "$PSScriptRoot\Export\ApkFiles\" -ItemType Directory -Force
 $progressBarForm = New-Object System.Windows.Forms.Form
-$progressBarForm.Size = New-Object System.Drawing.Size(300,200)
+$progressBarForm.Size = New-Object System.Drawing.Size(300, 400)
+$progressBarForm.StartPosition = "CenterScreen"
+$progressBarForm.Text = "Progress of Apk Processing"
+$progressLabel1 = New-Object System.Windows.Forms.Label
+$progressLabel1.Size = New-Object System.Drawing.Size(200, 25)
+$progressLabel1.Location = New-Object System.Drawing.Size(50, 25)
+$progressLabel1.Text = "Apk Pulling Progress"
 $progressBar = New-Object System.Windows.Forms.ProgressBar
-$progressBar.Size = New-Object System.Drawing.Size(200,25)
-$progressBar.Location = New-Object System.Drawing.Size(50,25)
+$progressBar.Size = New-Object System.Drawing.Size(200, 25)
+$progressBar.Location = New-Object System.Drawing.Size(50, 75)
 $progressBar.Visible = $true
 $progressBar.Maximum = $ParsedADBPaths.Count
 $progressBar.Minimum = 1
 $progressBar.Value = 1
+$progressLabel2 = New-Object System.Windows.Forms.Label
+$progressLabel2.Size = New-Object System.Drawing.Size(200, 25)
+$progressLabel2.Location = New-Object System.Drawing.Size(50, 150)
+$progressLabel2.Text = "Apk Processing Progress"
+$progressBar2 = New-Object System.Windows.Forms.ProgressBar
+$progressBar2.Size = New-Object System.Drawing.Size(200, 25)
+$progressBar2.Location = New-Object System.Drawing.Size(50, 200)
+$progressBar2.Visible = $true
+$progressBar2.Maximum = $ParsedADBPaths.Count
+$progressBar2.Minimum = 1
+$progressBar2.Value = 1
 $progressButton = New-Object System.Windows.Forms.Button
-$progressButton.Size = New-Object System.Drawing.Size(100,25)
-$progressButton.Location = New-Object System.Drawing.Size(100,75)
-$progressButton.Text = "Start Apk Pull"
+$progressButton.Size = New-Object System.Drawing.Size(100, 25)
+$progressButton.Location = New-Object System.Drawing.Size(100, 250)
+$progressButton.Text = "Start"
 $progressBarSB = {
     for ($i = 0; $i -lt $ParsedADBPaths.Count; $i++) {
         $Args3 = "pull", "$($ParsedADBPaths[$i])", "$PSScriptRoot\Export\ApkFiles\$($ParsedADBPackageNames[$i]).apk"
         & "$PSScriptRoot\Tools\adb.exe" @Args3
         $progressBar.Increment(1)
     }
+    $GetApks = Get-ChildItem -Path "$PSScriptRoot\Export\ApkFiles\" -File -Force
+    $CommonNames = @{}
+    foreach ($apk in $GetApks) {
+        $Args4 = "dump", "badging", "$($apk.FullName)"
+        $RawAAPT2Dump = & "$PSScriptRoot\Tools\aapt2.exe" @Args4
+        $ParsedDump = $RawAAPT2Dump | Select-String "(?<=application-label:')[^']+" | ForEach-Object { $_.Matches.Value }
+        $CommonNames.Add($apk.BaseName, $ParsedDump)
+    }
     $progressBarForm.Close()
 }
 $progressButton.Add_Click($progressBarSB)
 $progressBarForm.Controls.Add($progressBar)
+$progressBarForm.Controls.Add($progressLabel1)
+$progressBarForm.Controls.Add($progressBar2)
+$progressBarForm.Controls.Add($progressLabel2)
 $progressBarForm.Controls.Add($progressButton)
 $progressBarForm.ShowDialog()
-$GetApks = Get-ChildItem -Path "$PSScriptRoot\Export\ApkFiles\" -File -Force
-$CommonNames = @{}
-foreach ($apk in $GetApks) {
-    $Args4 = "dump", "badging", "$($apk.FullName)"
-    $RawAAPT2Dump = & "$PSScriptRoot\Tools\aapt2.exe" @Args4
-    $ParsedDump = $RawAAPT2Dump | Select-String "(?<=application-label:')[^']+" | ForEach-Object { $_.Matches.Value }
-    $CommonNames.Add($apk.BaseName,$ParsedDump)
-}
+
 $SortedApksByCommonName = $CommonNames.GetEnumerator() | Sort-Object -Property Value
 $SortedApksByCommonName | Out-GridView -Title "Apks & Names"
