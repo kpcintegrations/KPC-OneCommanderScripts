9946aef0090310f3582d58cd0df4ee8c31c4a1d5
Kyle Condon <admin@kpcintegrations.com>
2024-05-10 05:44:50 -0500

    Begining To Fix Scripts From Major OC Update

---
+++ KPC/Launch Terminal Interactively.ps1
@@ -0,0 +1,4 @@
+$Inputed = Read-Host -Prompt "Please Type Something!"
+Write-Host $Inputed
+$env:Selected_Files
+$env:CurrentDir
---
+++ KPC/LaunchTerminalInteractively.ps1
@@ -0,0 +1,5 @@
+$Inputed = Read-Host -Prompt "Please Type Something!"
+Write-Host $Inputed
+$env:Selected_Files
+$env:Current_Dir
+Get-ChildItem Env:
