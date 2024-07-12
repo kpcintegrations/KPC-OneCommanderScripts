6e2e87e3fb45956734a623f740855260fdc03b95
kpcintegrations <admin@kpcintegrations.com>
2024-07-11 20:42:34 -0500

    Fixed Interal Code a Bit

--- OC/Add-CurrentDirToEnvPath.ps1
+++ OC/Add-CurrentDirToEnvPath.ps1
@@ -1,7 +1,7 @@
 $OCVars = Get-OCVars
 $CurDir = $OCVars.CurrentDir
 $UserPath = [System.Environment]::GetEnvironmentVariable("PATH","User")
-$UserPath | Out-File -FilePath "C:\KyleTemp\BackUserPath.txt"
+$UserPath | Out-File -FilePath "D:\KyleTemp\BackUserPath.txt"
 $NewUserPath = ($UserPath + ";" + $CurDir)
 [System.Environment]::SetEnvironmentVariable("PATH",$NewUserPath,"User")
 
